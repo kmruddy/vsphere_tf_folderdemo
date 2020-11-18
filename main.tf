9e6dece26fb32e4015a996aba7efa4dc841d7fb4
kmruddy <kmruddy@gmail.com>
2020-11-18 17:24:50 -0500

    baseline for VCS workflow

--- main.tf
+++ main.tf
@@ -5,27 +5,27 @@ terraform {
       version = "1.24.2"
     }
   }
-
-  backend "remote" {
-    organization = "TPMM-Org"
-
-    workspaces {
-      name = "vSphere-ParentFolder-TampaVMUG"
-    }
-  }
 }
 
 provider "vsphere" {
-  user                 = "svc_tf@prob.local"
-  password             = "Terraform!23"
-  vsphere_server       = "probvcsa01.prob.local"
+  user                 = var.username
+  password             = var.password
+  vsphere_server       = var.vcenter
   allow_unverified_ssl = true
 }
 
-data "vsphere_datacenter" "dc" {}
+data "vsphere_datacenter" "dc" {
+  name = var.dc
+}
 
 resource "vsphere_folder" "parent" {
-  path          = "TampaVMUG"
+  path          = var.parent_folder
+  type          = "vm"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_folder" "web" {
+  path          = "${vsphere_folder.parent.path}/Web"
   type          = "vm"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "username" {
+  default = "svc_tf@prob.local"
+}
+
+variable "password" {
+  default = "Terraform!23"
+}
+
+variable "vcenter" {
+  default = "probvcsa01.prob.local"
+}
+
+variable "dc" {
+  default = "Prob-DC"
+}
+
+variable "parent_folder" {
+  default = "TampaVMUG"
+}
