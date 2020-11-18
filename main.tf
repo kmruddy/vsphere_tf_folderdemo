c6ed8fa629559ddaf4ede07073cd843077a79e6d
kmruddy <kmruddy@gmail.com>
2020-11-18 17:38:24 -0500

    update to full structure

--- main.tf
+++ main.tf
@@ -29,3 +29,39 @@ resource "vsphere_folder" "web" {
   type          = "vm"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
+
+resource "vsphere_folder" "db" {
+  path          = "${vsphere_folder.parent.path}/Databases"
+  type          = "vm"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_folder" "file_svr" {
+  path          = "${vsphere_folder.parent.path}/File Servers"
+  type          = "vm"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_folder" "mgmt" {
+  path          = "${vsphere_folder.parent.path}/Management"
+  type          = "vm"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_folder" "mgmt_win" {
+  path          = "${vsphere_folder.mgmt.path}/Windows"
+  type          = "vm"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_folder" "mgmt_linux" {
+  path          = "${vsphere_folder.mgmt.path}/Linux"
+  type          = "vm"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_folder" "mgmt_vmw" {
+  path          = "${vsphere_folder.mgmt.path}/VMware"
+  type          = "vm"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
