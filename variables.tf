11fe2cde5c8e3d9a3b8637409c85b8a70ad4f860
Kyle Ruddy <kmruddy@gmail.com>
2021-11-18 18:36:44 -0500

    update variables

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "username" {
+  default = "svc_tf@prob.local"
+}
+
+variable "password" {}
+
+variable "vcenter" {
+  default = "probvcsa01.prob.local"
+}
+
+variable "dc" {
+  default = "Prob-DC"
+}
+
+variable "parent_folder" {}
